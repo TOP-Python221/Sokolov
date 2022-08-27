7ca8825316ac425d191bd04d2ed2be5668973ee5
Sokoloff-M <109232419+Sokoloff-M@users.noreply.github.com>
2022-08-27 17:46:55 +0800

    Решена задача, с удалением одинаковых элементов списка

--- 06.05/1.py
+++ 06.05/1.py
@@ -1,16 +1,10 @@
 from random import randrange
 
-list_1 = []
+s_1 = []
 for i in range(10):
-    list_1.append(randrange(0, 10))
-list_2 = []
+    s_1.append(randrange(0, 10))
+s_2 = []
 for i in range(10):
-    list_2.append(randrange(0, 10))
+    s_2.append(randrange(0, 10))
 
-for i in range(list_1):
-    for j in range(list_2):
-        if i == j:
-            list_1.pop(i) and list_2.pop(j)
-
-print(list_1)
-print(list_2)
+print(list(set(s_1)-set(s_2)))
