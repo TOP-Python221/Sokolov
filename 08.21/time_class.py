647888d1ee63ae58b24c45ec8b1cee54b7d1a820
Sokoloff-M <109232419+Sokoloff-M@users.noreply.github.com>
2022-09-01 20:42:41 +0800

    Возможно я на верном пути)

---
+++ 08.21/time_class.py
@@ -0,0 +1,9 @@
+import datetime
+
+class Data:
+    def __init__(self,
+                 data: int,
+                 time_zone: int):
+        self.data = data
+        self.time_zone = time_zone
+
