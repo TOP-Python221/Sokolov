b38cf97ac2f66472aa36585fbe3a3a0fe437d800
Sokoloff-M <109232419+Sokoloff-M@users.noreply.github.com>
2022-09-06 23:30:59 +0800

    задача на время суток готова

--- 08.21/2.py
+++ 08.21/2.py
@@ -1,9 +1,21 @@
-import datetime
+from datetime import datetime
 
-class Data:
+class Data():
     def __init__(self,
                  data: int,
                  time_zone: int):
         self.data = data
         self.time_zone = time_zone
 
+    def time_zone(self):
+        return datetime.datetime.today()
+
+    def future_time(self):
+        return datetime.timedelta(days=1)
+
+
+d1 = Data.time_zone()
+print(d1.strftime("%A %d %B %Y"))
+
+d2 = Data.future_time()
+print(d2)
