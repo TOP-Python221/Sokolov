0bacb3871a835071a5c514055d09c763bb574e71
Sokoloff-M <109232419+Sokoloff-M@users.noreply.github.com>
2022-09-09 22:09:35 +0800

    задача 3 готова

---
+++ 06.05/2.2.py
@@ -0,0 +1,23 @@
+from random import choice, sample
+
+list1 = ['evil', 'dobro', 'telephone', 'oracle']
+
+lis = choice(list1)
+lisMix = sample(lis, k=len(lis))
+
+print('Загаданное слово: ' + ''.join(lisMix))
+
+while True:
+    guess = input('Введите своё слово: ')
+    if guess == lis:
+        print('Вы угадали! \nХотите ещё раз?')
+        what = input("Ваш ответ: ")
+        if what == "да":
+            lis = choice(list1)
+            lisMix = sample(lis, k=len(lis))
+            print('Загаданное слово: ' + ''.join(lisMix))
+            continue
+        else:
+            break
+    else:
+        print('Вы не угадали поробуте еще раз')
