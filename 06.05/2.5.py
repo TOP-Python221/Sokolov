46e9f81d64fb884f83c0dfd79e2810c9f08aa172
Sokoloff-M <109232419+Sokoloff-M@users.noreply.github.com>
2022-09-21 22:05:29 +0800

    Исправлена задача от 06_05_2 №2.5

--- 06.05/2.5.py
+++ 06.05/2.5.py
@@ -1,26 +1,10 @@
+data1 = {2: "Kirsten Cherry",
+	1: "Leigh Emerson"},
+data2 = {"third": "Kaseem Holt",
+	'four': "Henry Padilla",
+	'fifty': "Ivor Padilla"}
+
 end_dict = {}
-
-
-data = [
-	{
-		2: "Kirsten Cherry"
-	},
-	{
-		1: "Leigh Emerson"
-	},
-	{
-		"third": "Kaseem Holt"
-	},
-	{
-		'four': "Henry Padilla"
-	},
-	{
-		'fifty': "Ivor Padilla"
-	}
-]
-for k, v in data.items():
-	if k in end_dict:
-		end_dict[k].extend(v)
-	else:
-		end_dict[k] = v
+for k, v in data2.items():
+	end_dict[k] = v
 print(end_dict)
