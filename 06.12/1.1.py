2e0f276dd5cad5e84c307ccf977b5117348732b4
Sokoloff-M <109232419+Sokoloff-M@users.noreply.github.com>
2022-09-21 22:47:09 +0800

    Добавлено решение задачи 06_12_1 № 1.1

--- 06.12/1.1.py
+++ 06.12/1.1.py
@@ -1,2 +1,9 @@
-d = {3:'opa'}
-print(d)
+def ordinalDate(y, m, d):
+    y_is_leap = y % 400 == 0 or y % 4 == 0 and y % 100 != 0
+    month_days = [31, 28 + y_is_leap, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    md = 0
+    for i in range(m - 1):
+        md += month_days[i]
+    return md + d
+d, m, y =list(map(int, input('день, месяц, год: ').split()))
+print(f'Порядковый номер дня в году: {ordinalDate(d, m, y)}\n')
