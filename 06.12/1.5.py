26b615c13972587b660abc40de2c375ae7890882
Sokoloff-M <109232419+Sokoloff-M@users.noreply.github.com>
2022-10-05 22:37:38 +0800

    Добавлена задача от 06.12_1 1.96

---
+++ 06.12/1.5.py
@@ -0,0 +1,33 @@
+def Iter(a):
+    """Вычисляем набор средних значений итерируемого объекта"""
+    if a == type(list) or type(tuple):
+        return None
+
+    if a == type(str):
+        a.isdigit()
+        return a.split(' ')
+    sr_arif = 0
+
+    for a in range():
+        sr_arif = (a[0] + a[1]) / 2
+    print(sr_arif)
+
+    sr_geom = 0
+    for a in range():
+        sr_geom = (a[0] * a[1]) ** 0.5
+    print(sr_geom)
+
+    sr_harm = 0
+    for a in range():
+        sr_harm = (1/a[0] + 1/a[1]) /2
+    print(sr_harm)
+
+    sr_quadr = 0
+    for a in range():
+        sr_quadr = ((a[0]**2 + a[1]**2)**0.5) / 2
+    print(sr_quadr)
+
+print(Iter(52423))
+
+
+#застзастрял на решении, почему-то выводит только None
